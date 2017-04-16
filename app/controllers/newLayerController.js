7e91d88cc70ff329b1da585816ea4a93bd88524d
Luka <luka.glusica89@gmail.com>
2017-04-16 17:40:02 +0200

    Add new layer modal

---
+++ app/controllers/newLayerController.js
@@ -0,0 +1,35 @@
+angular.module("onlineGIS")
+  .controller("newLayerController", ["$scope","$rootScope", function($scope, $rootScope) {
+    $scope.geometryTypes = [{
+            id: 1,
+            label: "Point"
+        },
+        {
+            id: 2,
+            label: "Line"
+        },
+        {
+            id: 3,
+            label: "Polygon"
+        }
+    ];
+
+    $scope.layerModel = {
+        attributes: []
+    }
+    $scope.newAttribute = "";
+
+    $scope.addAttribute = function() {
+        $scope.layerModel.attributes.push($scope.newAttribute)
+    }
+
+    $scope.dismissNewLayer = function() {
+        $scope.$parent.$parent.dismiss();
+    }
+
+    $scope.saveNewLayer = function() {
+        //TODO:CREATE TABLE VIA REST - IF SUCCESSFULL, CLOSE MODAL, UPDATE LAYER CONTAINER, ELSE SHOW ERROR MESSAGE
+        $rootScope.layers.push($scope.layerModel);
+        $scope.$parent.$parent.close();
+    }
+  }])
