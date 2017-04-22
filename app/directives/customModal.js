90f5ac3a6c931bead089ecaad826ab4386ce8fa9
Luka <luka.glusica89@gmail.com>
2017-04-22 23:53:59 +0200

    Modify custom modal directive - custom listener support

--- app/directives/customModal.js
+++ app/directives/customModal.js
@@ -1,38 +1,48 @@
 angular.module("onlineGIS")
-    .directive("customModal", ["$uibModal", function($uibModal) {
-        return {
-            restrict: "A",
-            scope: {
-                customModalTitle: "@",
-                customModalBodyTemplate: "@",
-                customModalSize: "@",
-                customModalData: "="
-            },
-            link: function(scope, element) {
-
-                //OPEN BOOTSTRAP MODAL
-                function openModal() {
-                    $uibModal.open({
-                        templateUrl: "app/templates/modal.html",
-                        controller: "modalInstanceCtrl",
-                        size: scope.customModalSize,
-                        resolve: {
-                            data: {
-                                title: scope.customModalTitle,
-                                bodyTemplate: scope.customModalBodyTemplate,
-                                other: scope.customModalData
-                            }
-                        }
-                    });
-                }
-
-                //REGISTER ONCLICK EVENT
-                element.on("click", openModal);
-
-                //CLEANUP AFTER THE SCOPE IS DESTROYED - PREVENTS MEMORY LEAK
-                scope.$on("$destroy", function() {
-                  element.off("click", openModal);
-                })
+   .directive("customModal", ["$uibModal", "$rootScope", "$timeout", function($uibModal, $rootScope, $timeout) {
+      return {
+         restrict: "A",
+         scope: {
+            customModalTitle: "@",
+            customModalBodyTemplate: "@",
+            customModalSize: "@",
+            customModalData: "=",
+            customModalListener: "@"
+         },
+         link: function(scope, element) {
+            //OPEN BOOTSTRAP MODAL
+            function openModal(opt_data) {
+               $uibModal.open({
+                  templateUrl: "app/templates/modal.html",
+                  controller: "modalInstanceCtrl",
+                  size: scope.customModalSize,
+                  resolve: {
+                     data: {
+                        title: scope.customModalTitle,
+                        bodyTemplate: scope.customModalBodyTemplate,
+                        scopeData: scope.customModalData,
+                        other: opt_data
+                     }
+                  }
+               });
             }
-        }
-    }])
+
+            //REGISTER ONCLICK EVENT
+            element.on("click", openModal);
+
+            //REGISTER CUSTOM EVENT, IF PROVIDED
+            if(scope.customModalListener) {
+               $rootScope.$on(scope.customModalListener, function(ev, data) {
+                  openModal(data);
+               });
+            }
+
+            //CLEANUP AFTER THE SCOPE IS DESTROYED - PREVENTS MEMORY LEAK
+            scope.$on("$destroy", function() {
+               element.off("click", openModal);
+            });
+
+
+         }
+      }
+   }])
